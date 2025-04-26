b945f4718ce6d7f5177941a1e54ccaedc9f94da0
Aya Jandali <jandaliaya@gmail.com>
2025-04-26 19:57:57 +0300

    email verified at addded to user table

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -5,7 +5,7 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("Update your account's email address.") }}
         </p>
     </header>
 
@@ -17,11 +17,12 @@
         @csrf
         @method('patch')
 
-        <div>
+       {{-- <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
+--}}
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
