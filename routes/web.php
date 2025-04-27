fb16a6bc1510a06cd8452e09083faf954c26d6f7
Aya Jandali <jandaliaya@gmail.com>
2025-04-27 10:37:40 +0300

    routes update

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     
+    Route::get('super-employee/dashboard', [SuperEmployeeController::class, 'index'])
+        ->name('super-employee.dashboard');
+
     Route::post('/super-employee/leave-request', [LeaveRequestController::class, 'store'])
-    ->name('super-employee.leave-request');
+        ->name('super-employee.leave-request');
 
     Route::get('/dashboards/requests', [LeaveRequestController::class, 'index'])
         ->name('requests');
@@ -44,7 +49,5 @@ Route::middleware('auth')->group(function () {
 
 
 
-
-Route::middleware(['auth'])->get('/super-employee/dashboard', [SuperEmployeeController::class, 'index'])->name('super-employee.dashboard');
 
 require __DIR__.'/auth.php';
